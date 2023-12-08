0071f5c9b04a7dc3b0925aa7554b4f23230eef90
Andrew Huang <huan1715@purdue.edu>
2023-12-08 18:33:08 -0500

    Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+@app.get("/index/", response_class=HTMLResponse)
+async def table(request: Request):
+    response = templates.TemplateResponse("test2.html", {"request": request}) 
+    return response
